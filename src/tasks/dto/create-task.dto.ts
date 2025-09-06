3f1bb4c3cb76509ae9db0b7744a42ef97fa85176
lucas araujo <lucasao744@gmail.com>
2025-09-05 21:51:13 -0300

    pipe

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -4,7 +4,16 @@ DTO > Data Transfer Object (Objeto de Transferência de Dados)
  > se usa para representar quais dados e em que formatos em que determinada camada aceita ou retorna
 */
 
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateTaskDto {
+  @IsString({ message: 'O nome deve ser uma texto' })
+  @MinLength(4, { message: 'O nome deve ter no mínimo 4 caracteres' })
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   readonly description: string;
 }
